73116296b92ef03d19f58f65bb15b872c52b7779
G-Milzink <G.Milzink@gmail.com>
2022-09-29 17:03:01 +0000

    added partial functionallity for rps-game user input

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", rpsGame);
 
+/**
+ * Main "game-loop", called after loading the DOM
+ * waits for user input then starts rps game.
+ */
 function rpsGame(){
     var rpsCountdownTimer;
 
@@ -13,7 +17,7 @@ function rpsBegin(){
     document.getElementById("ai-choice").style.backgroundImage = "";
     document.getElementById("player-choice").style.backgroundImage = "";
     document.getElementById("rps-outcome-display").textContent = 3;
-    rpsCountdownTimer = setInterval(rpsCountdown,1000); 
+    rpsCountdownTimer = setInterval(rpsCountdown,1000);
 }
 
 /**
@@ -30,5 +34,27 @@ function rpsBegin(){
     }
     if (document.getElementById("rps-outcome-display").textContent === "Go!"){
         clearInterval(rpsCountdownTimer);
+        rpsAwaitUserInput();
+    }
+}
+
+function rpsAwaitUserInput(){
+    let selectors = document.getElementsByClassName("rps-selection");
+    for (selector of selectors){
+        selector.addEventListener("click", function(){
+            if (document.getElementById("rps-outcome-display").textContent === "Go!" && this.getAttribute("data-type") === "rock"){
+                document.getElementById("player-choice").style.backgroundImage = "url(assets/images/rps/rock.png)";
+                console.log("0");
+                // aiInput();
+            } else if (document.getElementById("rps-outcome-display").textContent === "Go!" && this.getAttribute("data-type") === "paper"){
+                document.getElementById("player-choice").style.backgroundImage = "url(assets/images/rps/paper.png)";
+                console.log("1");
+                // aiInput();
+            } else if (document.getElementById("rps-outcome-display").textContent === "Go!" && this.getAttribute("data-type") === "scissors"){
+                document.getElementById("player-choice").style.backgroundImage = "url(assets/images/rps/scissors.png)";
+                console.log("2");
+                // aiInput();
+            } 
+        })
     }
 }
