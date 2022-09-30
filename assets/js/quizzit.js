dbcfd375dbcab12912d8fb4ebb2ecb84c25f0f83
G-Milzink <G.Milzink@gmail.com>
2022-09-30 13:31:19 +0000

    created an array holding all riddles and their respective answers

---
+++ assets/js/quizzit.js
@@ -0,0 +1,7 @@
+// Create an array of riddles and answers.
+const RIDDLES = [];
+RIDDLES[0] = ["I'm rarely touched but often held. If you have wit, you'll use me well. What am I?","A Tongue"];
+RIDDLES[1] = ["I have seas with no water, coast with no sand, towns without people, mountains without land. What am I?","A Map"];
+RIDDLES[2] = ["Often will I spin a tale, never will I charge a fee. I'll amuse you an entire eve, but, alas you wont remember me. What am I?", "A Dream"];
+RIDDLES[3] = ["What does it have in it pocketsessss?", "A Ring"];
+RIDDLES[4] = ["I have roots that no one sees. I am taller then the trees. Up and up I go. And yet I never grow. What am i?", "A Mountain"];
