a24ec56067df7e138d6631f2a044f43e5635ceda
G-Milzink <G.Milzink@gmail.com>
2022-09-30 13:32:14 +0000

    created an array holding only 'false' answers

--- assets/js/quizzit.js
+++ assets/js/quizzit.js
@@ -5,3 +5,11 @@ RIDDLES[1] = ["I have seas with no water, coast with no sand, towns without peop
 RIDDLES[2] = ["Often will I spin a tale, never will I charge a fee. I'll amuse you an entire eve, but, alas you wont remember me. What am I?", "A Dream"];
 RIDDLES[3] = ["What does it have in it pocketsessss?", "A Ring"];
 RIDDLES[4] = ["I have roots that no one sees. I am taller then the trees. Up and up I go. And yet I never grow. What am i?", "A Mountain"];
+
+// Create array of "false" answers.
+const FALSE_ANSWERS = ["A Tree", "A Cloud", "A Journey",
+                                "A Hammer","The Sea", "The Sky",
+                                "A Foot", "An Arm", "A Leg",
+                                "A Vision", "A Lake", "An Egg",
+                                "The Grass", "The Ground", "A Sword"
+];
