3630cf4cf4636bcd0828ef36e1db3821d944b0e1
G-Milzink <G.Milzink@gmail.com>
2022-10-02 12:42:56 +0000

    added main 'game-loop'

--- assets/js/guillotine.js
+++ assets/js/guillotine.js
@@ -3,4 +3,15 @@ const guillotineGlobalVariables = {
     "answer" : ''
 }
 
+// Wait for the DOM to load, then execute main game function:
 document.addEventListener("DOMContentLoaded", runGuillotineGame);
+
+/**
+ * Main "game-loop"...called after loading the DOM.
+ */
+function runGuillotineGame(){   
+    guillotineGlobalVariables.answer = grabRandomWord();
+    buildKeyboard();
+    buildAnswerDisplay()
+    listenToKeyboard()
+}
