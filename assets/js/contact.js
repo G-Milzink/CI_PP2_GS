38c415aa762b5e808a7c753b5bf14e575ff9197a
G-Milzink <G.Milzink@gmail.com>
2022-10-03 14:27:34 +0000

    finalized emailjs functionallity (working)

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,10 @@
 function sendMessage(){
-    var tempParameters = {
+    var params = {
         from_name : document.getElementById("name").value,
-        message: document.getElementById("msg").value
+        email_id : document.getElementById("email").value,
+        message : document.getElementById("msg").value
     }
-
-
-    emailjs.send('service_6eisq0d','template_xeb75zg',tempParameters)
-    .then(function(res){
-        console.log("succes", res.status);
+    emailjs.send("service_6eisq0d","template_yf457nh",params).then(function(res){
     })
+    alert("message sent! Thank you! :)");
 }
