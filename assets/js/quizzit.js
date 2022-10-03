113a34cc41f55c9c53fc1ef87b88fbf4edcf1433
G-Milzink <G.Milzink@gmail.com>
2022-10-03 15:09:52 +0000

    moved 'game-data'to seperate .js file

--- assets/js/quizzit.js
+++ assets/js/quizzit.js
@@ -1,18 +1,3 @@
-// Create an array of riddles and answers.
-const RIDDLES = [];
-RIDDLES[0] = ["I'm rarely touched but often held. If you have wit, you'll use me well. What am I?","A Tongue"];
-RIDDLES[1] = ["I have seas with no water, coast with no sand, towns without people, mountains without land. What am I?","A Map"];
-RIDDLES[2] = ["Often will I spin a tale, never will I charge a fee. I'll amuse you an entire eve, but, alas you wont remember me. What am I?", "A Dream"];
-RIDDLES[3] = ["What does it have in it pocketsessss?", "A Ring"];
-RIDDLES[4] = ["I have roots that no one sees. I am taller then the trees. Up and up I go. And yet I never grow. What am i?", "A Mountain"];
-
-// Create array of "false" answers.
-const FALSE_ANSWERS = ["A Tree", "A Cloud", "A Journey",
-                                "A Hammer","The Sea", "The Sky",
-                                "A Foot", "An Arm", "A Leg",
-                                "A Vision", "A Lake", "An Egg",
-                                "The Grass", "The Ground", "A Sword"
-];
 
 // Wrap variables in named object to avoid global scope.
 const quizzitGlobalVariables = {
