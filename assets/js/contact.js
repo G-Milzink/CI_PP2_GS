c8702c78d64cb89d7023f406306447ae120ceffa
G-Milzink <G.Milzink@gmail.com>
2022-10-03 14:58:46 +0000

    added comments to multiple functions

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,7 +1,20 @@
+/**
+ * emailsjs script, taken from emailjs tutorial page.
+ */
 (function() {
     emailjs.init("nHsMv-fGFHGxcKPg5");
 })();
 
+
+/**
+ * Send information from contact form to my email adress according to template
+ * using emailJS
+ * 
+ * Code taken from:
+ * Contact Form with Email Js | Send Emails using JavaScript through Email Js | Email Js Tutorial
+ * by ALTERCODES
+ * https://www.youtube.com/watch?v=5EZsRnJpUNU&t=1s&ab_channel=ALTERCODES
+ */
 function sendMessage(){
     var params = {
         from_name : document.getElementById("name").value,
