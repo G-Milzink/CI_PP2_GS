824d87c4b71ffd1e193b0971f3213cf309d6ec6d
G-Milzink <G.Milzink@gmail.com>
2022-10-05 22:04:48 +0000

    fixed typo

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,7 +3,7 @@
  * Send information from contact form to my email adress according to template
  * using emailJS
  * 
- * Code taken from tutorial:
+ * Code taken (and adapted) from tutorial:
  * "Contact Form with Email Js | Send Emails using JavaScript through Email Js | Email Js Tutorial"
  * by ALTERCODES
  * https://www.youtube.com/watch?v=5EZsRnJpUNU&t=1s&ab_channel=ALTERCODES
@@ -23,25 +23,16 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     });       
 });
 
-
 /**
- * Code snippet taken from Stack Over Flow:
- * "Check if a string contains an email address?"
- * https://stackoverflow.com/questions/16424659/check-if-a-string-contains-an-email-address
- * @param {*} string
- * @returns true if string contains an email adress 
+ * Display a message to thank user for form-submission and provide link to homepage.
  */
-function checkIfEmailInString(string) { 
-    /\b[a-z0-9-_.]+@[a-z0-9-_.]+(\.[a-z0-9]+)+/i.test(string);
-}
-
 function displayMessage(){
-    let message = `
+    let modal = `
                     <div id="modal">
                         <h1>Thanks!</h1>
                         <h2>Your message has been seent!</h2>
                         <button id="back-home" onclick="window.location.href='index.html';">Take me back.</button>
                     </div>
     `;
-    document.getElementById("contact-wrapper").innerHTML = message;
+    document.getElementById("contact-wrapper").innerHTML = modal;
 }
